97dd154215fa7b8b05dee4fcb1cb31dc732e786b
GxJohan <johamax@gmail.com>
2025-09-18 18:39:59 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/model/Persona.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "personas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nombre;
+    
+    @Column(nullable = false)
+    private String apellido;
+    
+    private String email;
+    
+    private Integer edad;
+}
